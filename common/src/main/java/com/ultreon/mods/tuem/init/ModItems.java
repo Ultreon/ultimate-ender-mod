e9f331605a9ffe4275e76bc87848b45e773bfb95
ByteLaw5 <aradgal78@gmail.com>
2023-06-01 00:42:57 +0300

    Added the other stuff that wasn't committed

--- common/src/main/java/com/ultreon/mods/tuem/init/ModItems.java
+++ common/src/main/java/com/ultreon/mods/tuem/init/ModItems.java
@@ -1,2 +1,17 @@
-package com.ultreon.mods.tuem.init;public class ModItems {
+package com.ultreon.mods.tuem.init;
+
+import com.ultreon.mods.tuem.UltimateEnderMod;
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.Item;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(UltimateEnderMod.MOD_ID, Registries.ITEM);
+
+    public static final RegistrySupplier<Item> MUTATED_ENDERIUM_INGOT = ITEMS.register("mutated_enderium_ingot", () -> new Item(new Item.Properties()));
+
+    public static void register() {
+        ITEMS.register();
+    }
 }
