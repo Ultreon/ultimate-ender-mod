441d16f0c8ba550c454fc126f08157b1ed915cfb
ByteLaw5 <aradgal78@gmail.com>
2023-06-01 23:14:44 +0300

    Added mutated enderium tools and added block to tags

--- common/src/main/java/com/ultreon/mods/tuem/init/ModBlocks.java
+++ common/src/main/java/com/ultreon/mods/tuem/init/ModBlocks.java
@@ -4,18 +4,18 @@ import com.ultreon.mods.tuem.UltimateEnderMod;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 
 import java.util.function.Supplier;
 
 public class ModBlocks {
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(UltimateEnderMod.MOD_ID, Registries.BLOCK);
 
-    public static final RegistrySupplier<Block> MUTATED_ENDERIUM_BLOCK = register("mutated_enderium_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK)));
+    public static final RegistrySupplier<Block> MUTATED_ENDERIUM_BLOCK = register("mutated_enderium_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK)));
 
     @SuppressWarnings("SameParameterValue")
     private static <I extends Block> RegistrySupplier<I> register(String name, Supplier<I> supplier) {
